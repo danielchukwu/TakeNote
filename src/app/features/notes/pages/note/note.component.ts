075b05ca008754923e420e08f3dce680641bcb85
dc <00danzy@gmail.com>
2023-05-27 18:13:11 +0100

    add updateTitle, updateNoteHeader methods

--- src/app/features/notes/pages/note/note.component.ts
+++ src/app/features/notes/pages/note/note.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent {
+  
+  // NOTE TITLE
+  // This shows the edit form or not and shows the title or not
+  editTitleMode = false;
+  showTitleForm() { this.editTitleMode = !this.editTitleMode; }
 
+  updateTitle(field: HTMLInputElement) {
+    console.log("Save New Title: ", field.value)
+  }
+  
+  // NOTE HEADER TITLE
+  // This shows the edit form for the note header
+  editNoteHeaderMode = false;
+  showNoteHeaderForm() { this.editNoteHeaderMode = !this.editNoteHeaderMode; }
+
+  updateNoteHeader(field: HTMLInputElement) {
+    console.log("Save New Note Header: ", field.value)
+  }
+  
+  
+  addNewNote(value: string, form: HTMLFormElement) {
+    console.log("Save new note to the backend: ", value);
+    console.log(form);
+  }
 }
