27e0da34d06002efd0cd85cb134886001ba79e61
dc <00danzy@gmail.com>
2023-05-28 23:08:13 +0100

    add show panel capabilities

--- src/app/features/notes/pages/note/note.component.ts
+++ src/app/features/notes/pages/note/note.component.ts
@@ -6,26 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent {
+
+  // Show Side Panel or Not (for smaller screen sizes tablets and mobiles)
+  showSidePanel = false;
+  toggleSidePanel() { this.showSidePanel = !this.showSidePanel; }
   
-  // NOTE TITLE
-  // This shows the edit form or not and shows the title or not
+  // Note Title - This shows the edit form or not and shows the title or not
   editTitleMode = false;
   showTitleForm() { this.editTitleMode = !this.editTitleMode; }
-
   updateTitle(field: HTMLInputElement) {
     console.log("Save New Title: ", field.value)
   }
   
-  // NOTE HEADER TITLE
-  // This shows the edit form for the note header
+  // Note Header Title - This shows the edit form for the note header
   editNoteHeaderMode = false;
   showNoteHeaderForm() { this.editNoteHeaderMode = !this.editNoteHeaderMode; }
-
   updateNoteHeader(field: HTMLInputElement) {
     console.log("Save New Note Header: ", field.value)
   }
   
-  
+  // ADD - new note to data
   addNewNote(value: string, form: HTMLFormElement) {
     console.log("Save new note to the backend: ", value);
     console.log(form);
