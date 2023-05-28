3fd2dcc7ff4c7e22957e35398187bed84d969bfd
dc <00danzy@gmail.com>
2023-05-28 23:11:48 +0100

    complete

--- src/app/features/notes/components/display-svg/display-svg.component.ts
+++ src/app/features/notes/components/display-svg/display-svg.component.ts
@@ -7,18 +7,21 @@ import { Component, Input } from '@angular/core';
 })
 
 // To use this component all you have to do is specify an 
-// @svgName - is the name of the svg to be displayed e.g 'pen' | 'bin' | 'plus' | 'send'
+// @svgName - is the name of the svg to be displayed e.g 'pen' | 'todo' | 'bin' | 'plus' | 'send'
+// @svgSizeStyle - is a string that carries the size of the svg's class style  
 // @svgStyles - is a string that carries all the styles you want 
 export class DisplaySvgComponent {
   @Input() svgName = 'pen';
-  @Input() svgStyles = 'svg svg-12 pointer';
+  @Input() svgSizeStyle = '';
+  @Input() svgStyles = 'svg pointer';
+  @Input() svgStylesHover = 'pointer ';
   @Input() press = function(){};
 
   removeHoverStyle() {
-    this.svgStyles = 'svg svg-12 pointer';
+    this.svgStyles = 'svg pointer';
   }
   
   addHoverStyle() {
-    this.svgStyles = 'svg-12 pointer';
+    this.svgStyles = this.svgStylesHover;
   }
 }
