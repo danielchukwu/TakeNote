9e149f15e966e031866707137367781aab40a387
dc <00danzy@gmail.com>
2023-06-02 12:33:08 +0100

    add home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NoteComponent } from './features/notes/pages/note/note.component';
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { SignUpComponent } from './features/auth/pages/sign-up/sign-up.component';
+import { HomeComponent } from './features/home/pages/home/home.component';
 
 const routes: Routes = [
-  {path: '', component: NoteComponent},
+  {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: SignUpComponent},
 ];
