a0881d4e25850ccad2be7e75ee5b750befb92c51
dc <00danzy@gmail.com>
2023-06-04 23:14:48 +0100

    add model

---
+++ src/app/shared/models/note.ts
@@ -0,0 +1,8 @@
+export interface Note {
+  id: String;
+  avatar: String;
+  title: String;
+  description: String;
+  body: String;
+  updatedAt: String;
+}
---
+++ src/app/shared/models/notebook.ts
@@ -0,0 +1,7 @@
+export interface Notebook {
+  id: String;
+  avatar: String;
+  title: String;
+  description: String;
+  updatedAt: String;
+}
