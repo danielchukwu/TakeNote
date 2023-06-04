f4265b201b0ddb7dc51d936ec0237fbb629b9313
dc <00danzy@gmail.com>
2023-06-04 23:12:05 +0100

    first commit

---
+++ src/app/features/home/home-routing.module.ts
@@ -0,0 +1,22 @@
+import { authGuard } from "src/app/core/guards/auth.guard";
+import { NoteComponent } from "./components/note/note.component";
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { HomeComponent } from "./home.component";
+
+const routes: Routes = [
+  {
+    path: '', 
+    component: HomeComponent,
+    canActivate: [authGuard],
+    children: [
+      {path: 'n/:id', component: NoteComponent},
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
