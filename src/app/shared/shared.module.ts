6ccae94e5857fc01ed795f804625de254a89e170
dc <00danzy@gmail.com>
2023-06-05 21:27:54 +0100

    export AlertCardComponent for usage

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
+import { AlertCardComponent } from './components/alert-card/alert-card.component';
 
 
 
 @NgModule({
   declarations: [
-    CapitalizePipe
+    CapitalizePipe,
+    AlertCardComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
   ],
-  exports: [ CommonModule, FormsModule, CapitalizePipe ]
+  exports: [ AlertCardComponent, CommonModule, FormsModule, CapitalizePipe ]
 })
 export class SharedModule { }
