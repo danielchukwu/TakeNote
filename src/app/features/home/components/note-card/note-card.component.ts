8be912f2286c59308255a9f9006815b030bbe41c
dc <00danzy@gmail.com>
2023-06-07 23:45:58 +0100

    add updateNote feature complete - milestone 🦜

--- src/app/features/home/components/note-card/note-card.component.ts
+++ src/app/features/home/components/note-card/note-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Note } from 'src/app/shared/models/note';
+import { DataSharingService } from 'src/app/shared/services/data-sharing.service';
 
 @Component({
   selector: 'app-note-card',
@@ -6,13 +8,21 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./note-card.component.css']
 })
 export class NoteCardComponent {
-  @Input() note: any;
+  @Input() note: Note | any;
+
+  constructor(private dataSharingService: DataSharingService){}
   
-  delete() {
-    console.log('Delete Card: ', this.note);
+  updateNote() {
+    this.dataSharingService.setNoteToUpdate(this.note);
+    // Subscribe to the note to update so when it is updated we can update our current note card
+    this.dataSharingService.getUpdatedNote().subscribe((note) => {
+      if (note?.id === this.note.id && note?.body != this.note.body) {
+        this.note = note;
+      }
+    });
   }
   
-  edit() {
-    console.log('Edit Card: ', this.note);
+  deleteNote() {
+    console.log('Delete Card: ', this.note);
   }
 }
