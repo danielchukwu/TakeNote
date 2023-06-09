cd09237be2b472d8bcb348d9375a551f50755f32
dc <00danzy@gmail.com>
2023-06-09 22:04:14 +0100

    improve styles for side panel responsiveness

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataSharingService } from 'src/app/shared/services/data-sharing.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private dataSharingService: DataSharingService){}
+  ngOnInit(): void {
+    this.dataSharingService.setCloseSidebarPanel(this.toggleSidePanelOff());
+  }
 
   // Show Side Panel or Not (for smaller screen sizes tablets and mobiles)
   showSidePanel = false;
   toggleSidePanel() { this.showSidePanel = !this.showSidePanel; }
-  
+
+  // Mobile view only
+  // this function is passed down the component tree and it is used to close the sidebar panel when the child 
+  toggleSidePanelOff(){
+    const object = this;
+    const toggleSidePanel = () => object.showSidePanel = false;
+    return toggleSidePanel;
+  }
 }
