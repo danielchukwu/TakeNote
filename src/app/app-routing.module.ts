7a92be18891581a946706c9959ca9be03249cde7
dc <00danzy@gmail.com>
2023-06-13 21:58:37 +0100

    add WelcomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { SignUpComponent } from './features/auth/pages/sign-up/sign-up.component';
 import { authGuard, registerOrLoginGuard } from './core/guards/auth.guard';
+import { WelcomeComponent } from './features/welcome/welcome.component';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)},
+  {path: 'welcome', component: WelcomeComponent},
   {path: 'login', component: LoginComponent, canActivate: [registerOrLoginGuard]},
   {path: 'sign-up', component: SignUpComponent, canActivate: [registerOrLoginGuard]},
   {path: '**', redirectTo: "", pathMatch: "full", canActivate: [authGuard]},
