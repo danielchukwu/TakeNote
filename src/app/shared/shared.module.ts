28de9d1ef69e8eab582469e4e5726afd6e13f0bc
dc <00danzy@gmail.com>
2023-06-16 23:56:27 +0100

    add SquareButton, IconButton and Header Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,26 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
 import { AlertCardComponent } from './components/alert-card/alert-card.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
+import { SquareButtonComponent } from './components/buttons/square-button/square-button.component';
+import { IconButtonComponent } from './components/buttons/icon-button/icon-button.component';
 
 
 
 @NgModule({
   declarations: [
     CapitalizePipe,
-    AlertCardComponent
+    AlertCardComponent,
+    HeaderComponent,
+    SquareButtonComponent,
+    IconButtonComponent
   ],
   imports: [
+    RouterModule,
     CommonModule,
     FormsModule,
   ],
-  exports: [ AlertCardComponent, CommonModule, FormsModule, CapitalizePipe ]
+  exports: [ AlertCardComponent, HeaderComponent, IconButtonComponent, CommonModule, FormsModule, CapitalizePipe ]
 })
 export class SharedModule { }
