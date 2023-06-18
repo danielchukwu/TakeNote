5d0e59ebd817257bf0956146f0a7833199d4c65c
dc <00danzy@gmail.com>
2023-06-17 20:49:37 -0800

    update registerOrLoginGuard to unAuthenticatedGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { SignUpComponent } from './features/auth/pages/sign-up/sign-up.component';
-import { authGuard, registerOrLoginGuard } from './core/guards/auth.guard';
+import { authGuard, unAuthenticatedGuard } from './core/guards/auth.guard';
 import { WelcomeComponent } from './features/welcome/welcome.component';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)},
-  {path: 'welcome', component: WelcomeComponent},
-  {path: 'login', component: LoginComponent, canActivate: [registerOrLoginGuard]},
-  {path: 'sign-up', component: SignUpComponent, canActivate: [registerOrLoginGuard]},
+  // {path: 'welcome', component: WelcomeComponent, canActivate: [unAuthenticatedGuard]},
+  {path: 'login', component: LoginComponent, canActivate: [unAuthenticatedGuard]},
+  {path: 'sign-up', component: SignUpComponent, canActivate: [unAuthenticatedGuard]},
   {path: '**', redirectTo: "", pathMatch: "full", canActivate: [authGuard]},
 ];
 
