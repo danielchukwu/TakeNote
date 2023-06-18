3e75c12ed0ad8db4216d81d8050460ef9910dc49
dc <00danzy@gmail.com>
2023-06-17 20:44:51 -0800

    rename registerOrLoginGuard to unAuthenticatedGuard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -8,7 +8,7 @@ export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: R
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.getToken()) {
+  if (authService.isAuthenticated()) {
     return true;
   }
   router.navigate(["login"]);
@@ -17,11 +17,11 @@ export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: R
 
 // auth guard to prevent authenticated users from trying to 
 // access the login or sign up page without having logged out
-export const registerOrLoginGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+export const unAuthenticatedGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.getToken()) {
+  if (authService.isAuthenticated()) {
     router.navigate([""]);
     return false;
   }
