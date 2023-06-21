4a92f2acf0b2ff8be2fc7e36627697532180f584
dc <00danzy@gmail.com>
2023-06-21 22:15:04 +0100

    improve code

--- src/app/features/home/components/sidebar/sidebar.component.ts
+++ src/app/features/home/components/sidebar/sidebar.component.ts
@@ -16,32 +16,36 @@ export class SidebarComponent implements OnInit, OnDestroy {
   user: User = this.authService.getUser();
   notebooks: Notebook[] = [];
   selectedNotebook: Notebook | undefined;
-  
-  // subscription for fetching notebooks - 
-  // we don't use async pipe because we want to be able to add new notebooks to the 
+  showNotebookSkeleton = true;
+
+  // subscription for fetching notebooks -
+  // we don't use async pipe because we want to be able to add new notebooks to the
   // existing list which won't be possible otherwise if we used observables and async pipe
-  sub1$: Subscription | undefined;    
+  sub1$: Subscription | undefined;
 
   // subscription for updating selectCardId
-  sub2$: Subscription | undefined;     
+  sub2$: Subscription | undefined;
 
   constructor(
-    private authService: AuthService, 
-    private notebookService: NotebookService, 
+    private authService: AuthService,
+    private notebookService: NotebookService,
     private router: Router,
     private dataSharing: DataSharingService
   ) {}
   ngOnInit(): void {
     // get notebooks
-    this.sub1$ = this.notebookService.getNotebooks().subscribe((notebooks)=>{
+    this.sub1$ = this.notebookService.getNotebooks().subscribe((notebooks) => {
       this.notebooks = notebooks;
+      this.showNotebookSkeleton = false;
     });
 
     // updates the selected sidebar notebook id
-    this.sub2$ = this.dataSharing.getSelectedNotebook().subscribe((notebook) => {
-      console.log('New Notebook Selected');
-      this.selectedNotebook = notebook;
-    });
+    this.sub2$ = this.dataSharing
+      .getSelectedNotebook()
+      .subscribe((notebook) => {
+        console.log('New Notebook Selected');
+        this.selectedNotebook = notebook;
+      });
   }
   ngOnDestroy(): void {
     // remove subscription
@@ -53,28 +57,24 @@ export class SidebarComponent implements OnInit, OnDestroy {
     this.notebooks = this.notebooks.filter((notebook) => notebook.id !== id);
   }
 
-  // setSelectedCardId(id: String){ 
-  //   this.selectedCardId = id; 
-  //   this.closeSidebarMobile();
-  //   console.log('Function was called');
-  // }
-
   // CRUD
   updateAvatar() {
     alert('Update users avatar! 🦜');
   }
   createNoteBook() {
-    this.sub1$ = this.notebookService.createNotebook().subscribe((newNotebook) => {
-      console.log(newNotebook);
-      this.notebooks = [newNotebook, ...this.notebooks ];
-      // Open new notebook
-      this.dataSharing.setSelectedNotebook(newNotebook);
-      this.router.navigate(['n', newNotebook.id])
-    });
+    this.sub1$ = this.notebookService
+      .createNotebook()
+      .subscribe((newNotebook) => {
+        console.log(newNotebook);
+        this.notebooks = [newNotebook, ...this.notebooks];
+        // Open new notebook
+        this.dataSharing.setSelectedNotebook(newNotebook);
+        this.router.navigate(['n', newNotebook.id]);
+      });
   }
-  
+
   // logout
-  logout(){
+  logout() {
     this.authService.clear();
     this.router.navigate(['login']);
   }
