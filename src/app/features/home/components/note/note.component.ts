87d989c5da5e147e25c111c5abd04015def11066
dc <00danzy@gmail.com>
2023-06-21 22:14:27 +0100

    improve code

--- src/app/features/home/components/note/note.component.ts
+++ src/app/features/home/components/note/note.component.ts
@@ -11,11 +11,11 @@ import { NotebookService } from 'src/app/shared/services/notebook.service';
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.css']
+  styleUrls: ['./note.component.css'],
 })
 export class NoteComponent implements OnInit, OnDestroy {
   notes: Note[] = [];
-  notebook$: Observable<Notebook | undefined> = new Observable;
+  notebook$: Observable<Notebook | undefined> = new Observable();
   sub1$: Subscription[] = [];
   editTitleMode = false;
   editDescriptionMode = false;
@@ -24,42 +24,45 @@ export class NoteComponent implements OnInit, OnDestroy {
   showNoteSkeleton = true;
 
   constructor(
-    private notebookService: NotebookService, 
-    private noteService: NoteService, 
+    private notebookService: NotebookService,
+    private noteService: NoteService,
     private authService: AuthService,
     private route: ActivatedRoute,
     private dataSharingService: DataSharingService
   ) {}
-  
+
   ngOnInit(): void {
-    // First time this page loads, fetch the notebook from the database and set the 
+    // First time this page loads, fetch the notebook from the database and set the
     // selectedNotebook using our dataSharingService. So that whichever component needs this data can get it in real time
-    const sub$ = this.notebookService.getNotebook(this.route.snapshot.params['id']).subscribe((notebook) => {
-      this.dataSharingService.setSelectedNotebook(notebook);
-      this.showNotebookSkeleton = false;
-    });
-    // This runs whenever the url changes. 
+    const sub$ = this.notebookService
+      .getNotebook(this.route.snapshot.params['id'])
+      .subscribe((notebook) => {
+        this.dataSharingService.setSelectedNotebook(notebook);
+        this.showNotebookSkeleton = false;
+      });
+    // This runs whenever the url changes.
     // And when it does we want to fetch data for the new notebook
-    const urlChangeListenerSubscription$ = 
-      this.route.paramMap.subscribe((paramMap) => {
+    const urlChangeListenerSubscription$ = this.route.paramMap.subscribe(
+      (paramMap) => {
         const id = this.route.snapshot.params['id'];
         // Get notebooks notes list
-        this.notebookService
-          .getNotes(id)
-          .subscribe((notesList) => { 
-            this.notes = notesList; 
-            this.showNoteSkeleton = false; 
-          })
+        this.notebookService.getNotes(id).subscribe((notesList) => {
+          this.notes = notesList;
+          this.showNoteSkeleton = false;
+        });
 
-          // Update Selected Sidebar Notebook
-          this.notebook$ = this.dataSharingService.getSelectedNotebook();
-      });
+        // Update Selected Sidebar Notebook
+        this.notebook$ = this.dataSharingService.getSelectedNotebook();
+      }
+    );
     this.sub1$?.push(sub$);
     this.sub1$?.push(urlChangeListenerSubscription$);
   }
   ngOnDestroy() {
     // Unsubscribe from all the subscriptions
-    this.sub1$?.map((sub) => { sub.unsubscribe(); });
+    this.sub1$?.map((sub) => {
+      sub.unsubscribe();
+    });
   }
 
   removeNotebook(id: String) {
@@ -67,88 +70,120 @@ export class NoteComponent implements OnInit, OnDestroy {
   }
 
   // NOTE
-  
+
   // - Create
   createNote(value: String) {
     console.log(this.route.snapshot.params['id']);
 
     // add note
-    if (value.trim().length > 0 || value.trim() !== ''){
-      const sub$ = this.noteService.createNote({
-        body: value.trim(),
-        notebookId: this.route.snapshot.params['id'],
-        userId: this.authService.getUser().id
-      }).subscribe((newNote) => { this.notes = [newNote, ...this.notes] });
+    if (value.trim().length > 0 || value.trim() !== '') {
+      const sub$ = this.noteService
+        .createNote({
+          body: value.trim(),
+          notebookId: this.route.snapshot.params['id'],
+          userId: this.authService.getUser().id,
+        })
+        .subscribe((newNote) => {
+          this.notes = [newNote, ...this.notes];
+        });
 
       this.sub1$.push(sub$);
     }
   }
 
   // NOTEBOOK
-  
+
   // - Title
   //   show update title input field or not
-  showTitleForm() { this.editTitleMode = !this.editTitleMode; }
+  showTitleForm() {
+    this.editTitleMode = !this.editTitleMode;
+  }
   //   Update : higher-order functions
   updateTitle(): Function {
     const service = this.notebookService;
     const id = this.route.snapshot.params['id'];
     let title: String | undefined;
-    const sub$ = this.notebook$.subscribe((notebook) => {title = notebook?.title; });
+    const sub$ = this.notebook$.subscribe((notebook) => {
+      title = notebook?.title;
+    });
     this.sub1$.push(sub$);
 
     const update = (field: HTMLInputElement) => {
       // Check Validity
-      if (field.value.trim().length === 0 ){
-        this.dataSharingService.setAlert({ title: "Notebook description can't be empty 😕", isSuccess: false, showAlert: true});
+      if (field.value.trim().length === 0) {
+        this.dataSharingService.setAlert({
+          title: "Notebook description can't be empty 😕",
+          isSuccess: false,
+          showAlert: true,
+        });
         return;
       }
       // Check that title to be updated actually changed
-      if (title === field.value.trim()) { return; }
+      if (title === field.value.trim()) {
+        return;
+      }
 
-      return service.updateNotebook(id, {title: field.value.trim()}).subscribe((notebook: Notebook) => {
-        // Show successful alert
-        this.dataSharingService.setAlert({ title: 'Notebook was updated 💪', isSuccess: true, showAlert: true});
-        // Update
-        this.notebook$ = of(notebook);
-        this.dataSharingService.setSelectedNotebook(notebook);
-      });
-    }
+      return service
+        .updateNotebook(id, { title: field.value.trim() })
+        .subscribe((notebook: Notebook) => {
+          // Show successful alert
+          this.dataSharingService.setAlert({
+            title: 'Notebook was updated 💪',
+            isSuccess: true,
+            showAlert: true,
+          });
+          // Update
+          this.notebook$ = of(notebook);
+          this.dataSharingService.setSelectedNotebook(notebook);
+        });
+    };
     return update;
   }
 
   // - Description
   //   show update description input field or not
-  showDescriptionForm() { this.editDescriptionMode = !this.editDescriptionMode; }
+  showDescriptionForm() {
+    this.editDescriptionMode = !this.editDescriptionMode;
+  }
   //   update : higher-order functions
   updateDescription(): Function {
     const service = this.notebookService;
-    const id = this.route.snapshot.params['id']
+    const id = this.route.snapshot.params['id'];
     let description: String | undefined;
-    const sub$ = this.notebook$.subscribe((notebook) => {description = notebook?.description; });
+    const sub$ = this.notebook$.subscribe((notebook) => {
+      description = notebook?.description;
+    });
     this.sub1$.push(sub$);
 
     const update = (field: HTMLInputElement) => {
       // Check Validity
-      if (field.value.trim().length === 0 ){
-        this.dataSharingService.setAlert({ title: "Notebook description can't be empty 😕", isSuccess: false, showAlert: true});
+      if (field.value.trim().length === 0) {
+        this.dataSharingService.setAlert({
+          title: "Notebook description can't be empty 😕",
+          isSuccess: false,
+          showAlert: true,
+        });
         return;
       }
 
       // Check that title to be updated actually changed
-      if (description === field.value.trim()) { return; }
+      if (description === field.value.trim()) {
+        return;
+      }
 
-      return service.updateNotebook(id, {description: field.value.trim()}).subscribe((notebook: Notebook) => {
-        // Show successful alert
-        this.dataSharingService.setAlert({
-          title: 'Description was updated 💪',
-          isSuccess: true,
-          showAlert: true
+      return service
+        .updateNotebook(id, { description: field.value.trim() })
+        .subscribe((notebook: Notebook) => {
+          // Show successful alert
+          this.dataSharingService.setAlert({
+            title: 'Description was updated 💪',
+            isSuccess: true,
+            showAlert: true,
+          });
+          // Update
+          this.notebook$ = of(notebook);
         });
-        // Update
-        this.notebook$ = of(notebook);
-      });
-    }
+    };
     return update;
   }
 }
